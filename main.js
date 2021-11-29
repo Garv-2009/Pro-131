210a63a6295231571b3a556edec4730438b0feb3
Garv-2009 <75518344+Garv-2009@users.noreply.github.com>
2021-11-29 20:03:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function page1(){
+    window.location="lambo.html";
+}
+function page2(){
+    window.location="ferrari.html";
+}
+function page3(){
+    window.location="rr.html";
+}
+function page4(){
+    window.location="r6.html";
+}
---
+++ rr.js
@@ -0,0 +1,43 @@
+img = "";
+obj_detect = "";
+objects=[];
+function preload(){
+    img = loadImage("rr.jpeg");
+}
+function setup(){
+    canvas = createCanvas(500,500);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("obj_detect").innerHTML = "Status: Object Detecting";
+}
+function modelloaded(){
+  console.log("Model");
+  obj_detect = true;
+  objectDetector.detect(img,getResult);
+}
+function getResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+        
+    }
+    
+}
+function draw(){
+    image(img,0,0,500,500);
+    if(obj_detect != "" ){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("obj_detect").innerHTML = "Status: Rolls Royce Detected";
+            percent = floor(objects[i].confidence * 100);
+            fill("red");
+            text(objects[i].label + " " + percent + "%", objects[i].x , objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x -40 , objects[i].y, objects[i].height, objects[i].width)
+            
+        }
+    }
+}
